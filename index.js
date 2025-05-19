f97bc250bc8f1c2cf3510add68c30bdaa995435a
shazzed-76 <shazzed7656@gmail.com>
2025-05-19 19:37:06 +0600

    created user delete api

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
     })
 
 
+
+    app.delete('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await userColl.deleteOne(query);
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
